7b1a54ae84670dcea9c3f8732c3e80c1c42a73c6
Wojciech Grzelczak <wojciech.grzelczak@gmail.com>
2019-04-23 15:29:12 +0200

    Changed working directory for clang_format_one.py

--- Tools/clang_format_one.py
+++ Tools/clang_format_one.py
@@ -2,13 +2,19 @@ import os
 from os import path
 
 def clang_format(filePath):
+    previousWorkingDirectory = os.getcwd()    
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     extensions = [".cpp", ".hpp", ".c", ".h", ".inl"]
+
+    ret = False
     if any(filePath.endswith(ext) for ext in extensions):
         cmd = "clang-format -style=file -i {}".format(filePath)
         print(cmd)
         os.system(cmd)
-        return True
-    return False
+        ret = True
+
+    os.chdir(previousWorkingDirectory )
+    return ret
 
 
 if __name__=="__main__":
