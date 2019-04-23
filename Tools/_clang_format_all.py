31d6590d95da6ba3d95d93bec49b2bd41b4b3b87
Wojciech Grzelczak <wojciech.grzelczak@gmail.com>
2019-04-23 15:03:09 +0200

    Changed formating files

--- Tools/_clang_format_all.py
+++ Tools/_clang_format_all.py
@@ -5,9 +5,10 @@ from clang_format_one import clang_format
 
 currentDir = os.getcwd()
 slnDir = os.path.dirname(currentDir)
-gameDir = os.path.join(slnDir, "Game", "Nyngine")
+engineDir = os.path.join(slnDir, "Nyngine")
+gameDir = os.path.join(slnDir, "Game")
 
-for dir, dirs, files in chain.from_iterable(os.walk(path) for path in [gameDir]):
+for dir, dirs, files in chain.from_iterable(os.walk(path) for path in [engineDir, gameDir]):
     for file in files:
         filePath = os.path.join(dir, file)
         clang_format(filePath)
